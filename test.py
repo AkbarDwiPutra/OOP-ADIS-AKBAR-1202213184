65c81f418c586bd6572aedfd2f17313fafa12f23
AkbarDwiPutra <putradwiabay@student.telkomuniversity.ac.id>
2022-10-22 00:16:45 +0700

    praktikum oop

---
+++ test.py
@@ -0,0 +1,9 @@
+nama=input("Nama :")
+nim=input("NIM :")
+kelas=input("Kelas :")
+kode=input("Kode Asisten :")
+
+print ("Nama :", nama)
+print("NIM :", nim)
+print("Kelas :", kelas)
+print("Kode Asisten", kode)
