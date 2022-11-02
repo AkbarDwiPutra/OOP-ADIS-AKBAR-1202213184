0488c17d151ecb8f4d6d48720c8d78ee8c52ea83
AkbarDwiPutra <116377716+AkbarDwiPutra@users.noreply.github.com>
2022-11-02 15:46:18 +0700

    Add files via upload

---
+++ Menu.java
@@ -0,0 +1,18 @@
+public class Menu {
+    public String nama;
+    public String kategori;
+    public int price;
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    public void setKategori(String kategori) {
+        this.kategori = kategori;
+    }
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    // TODO Create Attribute of Menu; Name, Category, and Price then Create Sett
+
+}
