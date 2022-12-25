23ec6729b7a2a1a688e36f8f9c66674f539bc534
AkbarDwiPutra <116377716+AkbarDwiPutra@users.noreply.github.com>
2022-12-25 21:03:21 +0700

    Add files via upload

---
+++ MODUL1/src/User.java
@@ -0,0 +1,24 @@
+public class User {
+    public String nama;
+    public int telepon;
+
+    public void setNama(String nama){
+        this.nama = nama;
+    }
+    public void setTelepon(int telepon){
+        this.telepon = telepon;
+    }
+
+    public void register(){
+        System.out.println("Berhasil");
+        
+    }
+    
+
+
+    // TODO Create Attribute of User; Name and Phone Number then Create Setter
+
+    // TODO Create Method to Register User and Display User's Name and Phone Number and success message
+
+
+}
